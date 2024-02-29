331255c87635422e5d74ac14e9c345ffd9411db8
JuanViveros1 <juan.jose.viveros@correounivalle.edu.co>
2024-02-29 11:34:48 -0500

    nuevos cambios

---
+++ Clase 1/src/App.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        //JOptionPane.showMessageDialog(null,"Ya tengo hambre", "Eventos", JOptionPane.WARNING_MESSAGE);
+        byte edad = 127;
+        System.out.println(edad);
+        float altura = 1.71f;
+        System.out.println(altura);
+        boolean tengoHambre = true;
+        System.out.println(tengoHambre);
+        char inicial = 'J';
+        System.out.println(inicial);
+        String nombre = "Juan";
+        System.out.println(nombre);
+        if (5<2 && 3>2){
+            System.out.println(":)");
+        }
+    }
+}
